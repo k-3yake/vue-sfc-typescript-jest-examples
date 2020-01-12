08e502d76cc5bcd45558c7dad56c523fae13c504
katsuki.miyake <katsuki.miyake@uzabase.com>
2020-01-12 23:49:27 +0900

    TSのテスト実行出来た

--- src/components/__tests__/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
-
-describe('Component', () => {
-    test('is a Vue instance', () => {
-        const wrapper = mount(HelloWorld)
-        expect(wrapper.isVueInstance()).toBeTruthy()
-    })
-})
